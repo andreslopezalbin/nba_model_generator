7544d7dbc7b7aa1f8a599997d44b6af3ae138bfd
Andrés López Albín <andres.lopezalbin.external@ts.fujitsu.com>
2020-05-03 16:13:50 +0200

    Added training fuction

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ### Serverless commands to create a service with python 3.8 template in the folder ./myService
 serverless create --template aws-python3 --path myService
+
+### Deploy service to AWS Lambda
+sls deploy
+serverless deploy
--- predictionService/handler.py
+++ predictionService/handler.py
@@ -1,16 +1,112 @@
 import json
 import pymongo
 import pickle
+import os
+import boto3
+import io
+import datetime
+
 import pandas as pd
 import numpy as np
-import os
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
 
 db_url = os.environ['db_url']
 
 
-def hello(event, context):
+def train(event, context):
+    s3 = boto3.client('s3')
+    print("Getting S3 object...")
+    dataset = s3.get_object(Bucket='nba-datasets-bucket', Key='train.csv')
+
+    train_df = pd.read_csv(io.BytesIO(dataset['Body'].read()))
+    print(train_df.head())
+
+    train_df = train_df[(train_df.HOME_TEAM_ID.notnull()) & (train_df.VISITOR_TEAM_ID.notnull())]
+    train_df = train_df.drop(
+        ['HOME_WL', 'VISITOR_WL', 'HOME_PTS', 'VISITOR_PTS', 'GAME_DATE', 'MATCHUP', 'HOME_TEAM_ABBREVIATION',
+         'VISITOR_TEAM_ABBREVIATION', 'HOME_TEAM_NAME', 'VISITOR_TEAM_NAME'], axis=1)
+
+    train_df = train_df[(train_df.HOME_TEAM_ID.notnull()) & (train_df.VISITOR_TEAM_ID.notnull())]
+    train_df = train_df.astype({'HOME_TEAM_ID': 'int64', 'VISITOR_TEAM_ID': 'int64'})
+
+    for col in train_df.columns:
+        nullsInCol = train_df[col].isna().sum()
+        # print('nulls in col ', col, nullsInCol)
+        if nullsInCol > 0:
+            media_col = train_df[[col]].mean()
+            train_df[col] = train_df[[col]].fillna(media_col)
+
+    y = train_df['PLUS_MINUS']
+    X = train_df.drop(['PLUS_MINUS'], axis=1)
+
+    not_standardize = ['GAME_ID', 'SEASON_ID', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID']
+    for col in X.columns:
+        if col not in not_standardize:
+            X[col] = StandardScaler().fit_transform(X[[col]])
+
+    regresor = LinearRegression()
+    regresor.fit(X, y)
+
+    score = cross_val_score(regresor, X, y, cv=10, scoring='neg_mean_absolute_error')
+    mae = score.mean()
+    score = cross_val_score(regresor, X, y, cv=10, scoring='neg_mean_squared_error')
+    mse = score.mean()
+    score = cross_val_score(regresor, X, y, cv=10, scoring='r2')
+    r2 = score.mean()
+
+    print('MAE:', mae)
+    print('MSE:', mse)
+    print('R2:', r2)
+
+    pickled_model = pickle.dumps(regresor)
+
+    print("connecting to DB")
+
+    client = pymongo.MongoClient(db_url)
+    nba_models = client.nbaDB.models
+    active_model = nba_models.find_one({"active": True})
+
+    active = False
+    message = ''
+
+    if active_model is None:
+        active = True
+        message = 'Active model not found'
+        print(message)
+    elif active_model['scores']['R2'] < r2:
+        active = True
+
+        nba_models.update_one({'_id': active_model['_id']}, {"$set": {"active": False}})
+        message = ' Better model found. Scores R2: ' + str(r2) + ' --- MAE: ' + str(mae) + ' --- MSE: ' + str(mse)
+        print(message)
+    else:
+        message = ' Better model NOT found. Active model scores R2: ' + active_model['scores']['R2'] \
+                   + ' --- MAE: ' + active_model['scores']['MAE'] \
+                   + ' --- MSE: ' + active_model['scores']['MSE'] \
+                   + ' ### NEW model scores R2: ' + str(r2) \
+                   + ' --- MAE: ' + str(mae) \
+                   + ' --- MSE: ' + str(mse)
+
+        print(message)
+
+    model = {
+        "model": pickled_model,
+        "date": datetime.datetime.utcnow(),
+        "active": active,
+        "scores": {
+            "R2": r2,
+            "MAE": mae,
+            "MSE": mse
+        }
+    }
+
+    print('Saving model on DB')
+    nba_models.insert_one(model)
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": message,
         "input": event
     }
 
