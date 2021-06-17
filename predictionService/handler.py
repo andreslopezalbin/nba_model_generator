beced9cb43245873e580dc626a68288301c17acd
Andrés López Albín <andreslopezalbin@gmail.com>
2021-06-17 16:43:00 +0200

    removed game_id and season_id from training/predicting

--- predictionService/handler.py
+++ predictionService/handler.py
@@ -32,8 +32,8 @@ def train(event, context):
 
     # Limpieza de literales y parámetros que pueden interferir con los resultados del experimento.
     train_df = train_df.drop(
-        ['HOME_WL', 'VISITOR_WL', 'HOME_PTS', 'VISITOR_PTS', 'GAME_DATE', 'MATCHUP', 'HOME_TEAM_ABBREVIATION',
-         'VISITOR_TEAM_ABBREVIATION', 'HOME_TEAM_NAME', 'VISITOR_TEAM_NAME'], axis=1)
+        ['GAME_ID', 'SEASON_ID', 'HOME_WL', 'VISITOR_WL', 'HOME_PTS', 'VISITOR_PTS', 'GAME_DATE', 'MATCHUP',
+         'HOME_TEAM_ABBREVIATION', 'VISITOR_TEAM_ABBREVIATION', 'HOME_TEAM_NAME', 'VISITOR_TEAM_NAME'], axis=1)
 
     # Se eliminan los registros que no dispongan de los datos de los dos equipos
     train_df = train_df[(train_df.HOME_TEAM_ID.notnull()) & (train_df.VISITOR_TEAM_ID.notnull())]
@@ -47,7 +47,7 @@ def train(event, context):
             train_df[col] = train_df[[col]].fillna(media_col)
 
     # Se recorre cada una de las columnas escalando sus valores.
-    not_standardize = ['GAME_ID', 'SEASON_ID', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID', 'PLUS_MINUS']
+    not_standardize = [ 'HOME_TEAM_ID', 'VISITOR_TEAM_ID', 'PLUS_MINUS']
     for col in train_df.columns:
         if col not in not_standardize:
             train_df[col] = StandardScaler().fit_transform(train_df[[col]])
@@ -129,11 +129,11 @@ def predict(event, context):
 
     df = get_dataset()
     df = df.drop(
-        ['HOME_WL', 'VISITOR_WL', 'HOME_PTS', 'VISITOR_PTS', 'MATCHUP', 'HOME_TEAM_NAME', 'VISITOR_TEAM_NAME',
-         'PLUS_MINUS'], axis=1)
+        ['GAME_ID', 'SEASON_ID', 'HOME_WL', 'VISITOR_WL', 'HOME_PTS', 'VISITOR_PTS', 'MATCHUP', 'HOME_TEAM_NAME',
+         'VISITOR_TEAM_NAME', 'PLUS_MINUS'], axis=1)
 
-    local = 'TOR'
-    visitor = 'LAL'
+    local = event.local
+    visitor = event.visitor
     local_games = df.loc[df['HOME_TEAM_ABBREVIATION'] == local]
     local_games = local_games.sort_values(by="GAME_DATE", ascending=False)
     visitor_games = df.loc[df['VISITOR_TEAM_ABBREVIATION'] == visitor]
@@ -143,7 +143,7 @@ def predict(event, context):
     visitor_games = visitor_games.drop([col for col in local_games.columns if 'HOME_' in col], axis=1)[:10]
 
     local_games = local_games.drop(['GAME_DATE', 'HOME_TEAM_ABBREVIATION'], axis=1)
-    visitor_games = visitor_games.drop(['GAME_ID', 'SEASON_ID', 'GAME_DATE', 'VISITOR_TEAM_ABBREVIATION'], axis=1)
+    visitor_games = visitor_games.drop(['GAME_DATE', 'VISITOR_TEAM_ABBREVIATION'], axis=1)
 
     columns = [*local_games.columns, *visitor_games.columns]
     data = [[*local_games.mean(), *visitor_games.mean()]]
